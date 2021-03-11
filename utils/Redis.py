425a71665f8133348a2b1070b98bf861fcdbfbb3
wanghua35 <wh970305@163.com>
2021-03-11 13:13:52 +0800

    test

--- utils/Redis.py
+++ utils/Redis.py
@@ -8,14 +8,20 @@ from utils.Singleton import singleton
 
 @singleton
 class Redis:
+    config = dict()
+
     def __init__(self):
-        Redis.pool = redis.ConnectionPool(
-            host=config('REDIS_HOST'),
-            port=config('REDIS_PORT'),
-            db=config('REDIS_DB_INDEX', 0),
-            password=config('REDIS_PASSWORD'),
-            decode_responses=True)
+        self.init_config()
+        Redis.pool = redis.ConnectionPool(**self.config)
         self.db = redis.Redis(connection_pool=Redis.pool)
+
+    def init_config(self):
+        self.config['host'] = config('REDIS_HOST')
+        self.config['port'] = config('REDIS_PORT', 6379)
+        self.config['db'] = config('REDIS_DB_INDEX', 0)
+        self.config['decode_responses'] = True
+        if config('REDIS_PASSWORD'):
+            self.config['password'] = config('REDIS_PASSWORD')
 
 
 if __name__ == '__main__':
